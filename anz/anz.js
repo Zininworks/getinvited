947e5f25bdb6650f215ec03571b633b32eb9843d
Ahn Jungho air <ahnjungho-air@Ahnui-MacBook-Air.local>
2014-05-30 17:28:42 +0900

    redesign anz

--- anz/anz.js
+++ anz/anz.js
@@ -4,5 +4,5 @@ var iOS = (useragent.match(/(iPad|iPhone|iPod)/g) ? true : false );
 var isSafariVersion = (useragent.match(/Version\/(4.0|4.0.4|4.0.5|5.0.2|5.1|6.0|7.0)/g) ? true : false);
 
 if(iOS&isSafariVersion){
-    $("#ics").css("display", "block");
+    $("#ics").css("visibility", "visible");
 }
